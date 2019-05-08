92d88ba874b64ec0b60f2f0c0204718a45e8454d
Christian Nytra <ch.nytra.13@gmail.com>
2019-05-08 15:29:18 +0200

    Initial Commit

---
+++ MTG.WinCalculator/MTG.WinCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG.WinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
